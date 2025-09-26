744ced8dc29e88e3757e2c05fe5bc7646208d761
ZUBER Lionel Armaklan <lionel.zuber@armaklan.org>
2025-09-26 16:14:17 +0200

    Mémorisation de l'url pour gestion du back

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,14 +1,11 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withHashLocation } from '@angular/router';
 import { routes } from './app.routes';
-import { LocationStrategy } from '@angular/common';
-import { NoopLocationStrategy } from './noop-location.strategy';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
-    provideRouter(routes),
-    { provide: LocationStrategy, useClass: NoopLocationStrategy }
+    provideRouter(routes, withHashLocation())
   ]
 };
